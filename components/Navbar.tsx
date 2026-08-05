47a400017f71f42c85a480ae17933129e96cdf59
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-05 16:50:45 -0700

    refactor: style Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,14 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="sticky top-0 grid grid-cols-3 items-center border-b py-3 px-8 text-lg font-bold">
       <div>
-        <Link href="/">Home</Link>
-        <Link href="/projects">Projects</Link>
+        <Link className="hover:underline underline-offset-4 decoration-2" href="/">HOME</Link>
+      </div>
+      <div className="flex justify-self-center justify-between w-full max-w-md">
+        <Link className="hover:underline underline-offset-4 decoration-2" href="/projects">PROJECTS</Link>
+        <Link className="hover:underline underline-offset-4 decoration-2" href="/gym">GYM</Link>
+        <Link className="hover:underline underline-offset-4 decoration-2" href="/me">ABOUT ME</Link>
       </div>
     </nav>
   );
