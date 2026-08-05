658435cdb0108eca89c93e85f83e0f0c025bfa3a
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-05 13:12:46 -0700

    feat: implement project summary component with dynamic rendering

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,42 +1,95 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import './main-styles.css';
 import Link from 'next/link';
 
+interface Phase {
+  comment: string;
+  description: string;
+}
+
+interface ProjectData {
+  title: string;
+  summary: string;
+  phases: Phase[];
+  stack: string[];
+}
+
+interface ProjectSummariesProps {
+  projectSummaries: ProjectData[];
+}
+
+function ProjectSummary({ title, summary, phases, stack }:ProjectData) {
+  return (
+    <div className="project-summary-container">
+      <h2 className="w-fit">
+        <Link href="/projects/this" className="hover:underline underline-offset-4">Project Summary: {title}</Link>
+      </h2>
+
+      <h3>Goal</h3>
+      <p>{summary}</p>
+
+      <h3>Approach & Roadmap</h3>
+      <ul>{phases.map((phase, index) => {
+        return (
+          <li>Phase {index+1}: {phase.description}</li>
+        );
+      })}
+      </ul>
+
+      <h3>Tech Stack & Implementation</h3>
+      <ul>{stack.map((str) => {
+        return (
+          <li>{str}</li>
+        );
+      })}
+      </ul>
+    </div>
+  );
+}
+
+function ProjectSummaries({projectSummaries}:ProjectSummariesProps) {
+  return (
+    projectSummaries.map((projectSummary) => {
+      return <ProjectSummary title={projectSummary.title} summary={projectSummary.summary} phases={projectSummary.phases} stack={projectSummary.stack}/>
+    })
+  );
+}
+
 export default function ProjectPage() {
-return (
+  const projectSummaries = [
+    {
+      title: 'Personal Life Dashboard',
+      summary: 'Build a personal website to display project work and track daily metrics (Gym, Running, Sleep). The secondary goal is to learn web development by shipping a functional, end-to-end system with minimal prior experience.',
+      phases: [
+        {
+          comment: 'Static Site',
+          description: 'Begin strictly with a static Projects page to establish the base site before introducing metric dashboards.'
+        }, {
+          comment: 'Gym Data',
+          description: 'Add gym logging next using manual entry (eg. Google Sheets or JSON) since it has no external API dependencies'
+        }, {
+          comment: 'Garmin Data',
+          description: 'Add Running and Sleep dashboards later. Start with manual Garmin data exports, then explore automated options using unofficial APIs in future updates.'
+        }, {
+          comment: 'Defer Complex Features',
+          description: 'Postpone database setup, user authentication, and live interactive forms until the initial read-only site is deployed and running smoothly.'
+        }
+      ],
+      stack: [
+        'Framework: Next.js',
+        'Hosting: Vercel',
+        'Data Sources: Static JSON files or Google Sheets imports initially; CSV/JSON manual exports for Garmin data.'
+      ]
+    },
+  ];
+
+  return (
     <main>
-      <title>Lucas' site</title>
-      <header className="page-header">
-          Projects
-        </header>
+      <header className="page-header">Projects</header>
 
-      <div className="project-summary-container">
-        <h2 className="w-fit">
-          <Link href="/projects/this" className="hover:underline underline-offset-4">Project Summary: Personal Life Dashboard</Link>
-        </h2>
-        <h3>Goal</h3>
-        <p>
-          Build a personal website to display project work and track daily metrics (Gym, Running, Sleep). The secondary goal is to
-          learn web development by shipping a functional, end-to-end system with minimal prior experience.
-        </p>
-
-        <h3>Approach & Roadmap</h3>
-        <ul>
-          <li> Phase 1 (Static Site): Begin strictly with a static Projects page to establish the base site before introducing metric dashboards.</li>
-          <li> Phase 2 (Gym Data): Add gym logging next using manual entry (e.g., Google Sheets or JSON) since it has no external API dependencies.</li>
-          <li>Phase 3 (Garmin Data): Add Running and Sleep dashboards later. Start with manual Garmin data exports, then explore automated options using unofficial APIs in future updates.</li>
-          <li>Defer Complex Features: Postpone database setup, user authentication, and live interactive forms until the initial read-only site is deployed and running smoothly.</li>
-        </ul>
-
-        <h3>Tech Stack & Implementation</h3>
-        <ul>
-          <li>Framework: Next.js</li>
-          <li>Hosting: Vercel</li>
-          <li>Data Sources: Static JSON files or Google Sheets imports initially; CSV/JSON manual exports for Garmin data.</li>
-        </ul>
-      </div>
+      <ProjectSummaries projectSummaries={projectSummaries}/>
     </main>
   );
 }
