dd7fa8e8ce6bc81048b9aa8b71d7cd372ce2e13c
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-05 14:43:35 -0700

    feat: remove main-styles.css, turning it into Tailwind

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from 'react';
-import './main-styles.css';
 import Link from 'next/link';
 
 interface Phase {
@@ -22,26 +21,26 @@ interface ProjectSummariesProps {
 
 function ProjectSummary({ title, summary, phases, stack }:ProjectData) {
   return (
-    <div className="project-summary-container">
-      <h2 className="w-fit">
-        <Link href="/projects/this" className="hover:underline underline-offset-4">Project Summary: {title}</Link>
+    <div className="border rounded-lg px-5 pb-4">
+      <h2 className="w-fit text-3xl font-bold my-5">
+        <Link href="/projects/this" className="hover:underline underline-offset-8">Project Summary: {title}</Link>
       </h2>
 
-      <h3>Goal</h3>
+      <h3 className="font-bold text-xl mt-5 mb-2.5">Goal</h3>
       <p>{summary}</p>
 
-      <h3>Approach & Roadmap</h3>
-      <ul>{phases.map((phase, index) => {
+      <h3 className="font-bold text-xl mt-5 mb-2.5">Approach & Roadmap</h3>
+      <ul className="list-disc list-outside pl-10 space-y-2">{phases.map((phase, index) => {
         return (
           <li key={index}>Phase {index+1}{(phase.comment) ? ` (${phase.comment})`: ''}: {phase.description}</li>
         );
       })}
       </ul>
 
-      <h3>Tech Stack & Implementation</h3>
-      <ul>{stack.map((str, index) => {
+      <h3 className="font-bold text-xl mt-5 mb-2.5">Tech Stack & Implementation</h3>
+      <ul className="list-disc list-outside pl-10 space-y-2">{stack.map((str, index) => {
         return (
-          <li key={index}>{str}</li>
+          <li key={index} className="mb-2">{str}</li>
         );
       })}
       </ul>
@@ -86,9 +85,11 @@ export default function ProjectPage() {
   ];
 
   return (
-    <main>
-      <header className="page-header">Projects</header>
-      <ProjectSummaries projectSummaries={projectSummaries}/>
-    </main>
+    <body className="bg-[rgb(240,246,250)]">
+      <main className="my-[10vh] mx-[10vw]">
+        <header className="font-bold text-5xl mb-10">Projects</header>
+        <ProjectSummaries projectSummaries={projectSummaries}/>
+      </main>
+    </body>
   );
 }
