2b7be4bf4c50712631e379ca4daf167b0b7196ec
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-06 21:33:48 -0700

    Add form submission handling

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, SubmitEvent } from 'react';
 
 interface FormData {
   title: string;
@@ -22,19 +22,26 @@ export default function JournalFormPage() {
     }));
   };
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Submitted Data:', {formData});
+    setFormData({title: '', date: '', content: ''});
+  }
+
   return (
-    <form>
-      <div>
+    <form onSubmit={handleSubmit}>
+      <div className="block">
         <label>Title</label>
         <input name='title' value={formData.title} onChange={handleChange}></input>
       </div>
-      <div>
+      <div className="block">
         <label>Date</label>
         <input name='date' value={formData.date} onChange={handleChange}></input>
       </div>
-      <div>
+      <div className="block">
         <input></input>
       </div>
+      <button type="submit" className="cursor-pointer">Submit</button>
     </form>
   );
 }
