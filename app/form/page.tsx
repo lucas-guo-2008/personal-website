366f29da4c670e7895b2f48b44dd609e6c656e91
Lucas Guo <lucas.lu.guo@gmail.com>
2026-08-07 12:41:22 -0700

    Ensure all form fields working, add some styling

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -48,31 +48,35 @@ export default function JournalFormPage() {
   }, []);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="block">
-          <label>Title</label>
-          <input name='title' value={formData.title} onChange={handleChange}></input>
-        </div>
-        <div className="block">
-          <label>Date</label>
-          <input name='date' value={formData.date} onChange={handleChange}></input>
-        </div>
-        <div className="block">
-          <input></input>
-        </div>
-        <button type="submit" className="cursor-pointer">Submit</button>
-      </form>
-
-      <div>
-        {entries.map((entry) => (
-          <div key={entry.title}>
-            <div>{entry.title}</div>
-            <div>{entry.date}</div>
-            <div>{entry.content}</div>
-            <button onClick={() => handleDelete(entry.title)}>Delete</button>
+    <div className="bg-[rgb(240,246,250)] flex-1">
+      <div className="py-[10vh] px-[10vw]">
+        <h1 className="font-bold text-5xl mb-10">Project Log Submission</h1>
+        <form onSubmit={handleSubmit} className="border rounded-lg px-5 pb-4">
+          <div className="w-fit text-2xl font-bold my-5">
+            <label className="block">Title</label>
+            <input name='title' value={formData.title} onChange={handleChange} className="border"></input>
           </div>
-        ))}
+          <div className="w-fit text-2xl font-bold my-5">
+            <label className="block">Date</label>
+            <input name='date' value={formData.date} onChange={handleChange}className="border"></input>
+          </div>
+          <div className="w-fit text-2xl font-bold my-5">
+            <label className="block">Content</label>
+            <input name='content' value={formData.content} onChange={handleChange} className="border"></input>
+          </div>
+          <button type="submit" className="cursor-pointer border">Submit</button>
+        </form>
+
+        <div>
+          {entries.map((entry) => (
+            <div key={entry.title}>
+              <div>{entry.title}</div>
+              <div>{entry.date}</div>
+              <div>{entry.content}</div>
+              <button onClick={() => handleDelete(entry.title)} className="cursor-pointer">Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
